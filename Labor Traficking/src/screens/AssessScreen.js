f8aca6cefc914f3d41be94728706555f7b21ad88
Grace <gyoon18@bu.edu>
2021-07-01 13:24:21 -0400

    Adjusted Assess header, adjusted grid menu layout

--- Labor Traficking/src/screens/AssessScreen.js
+++ Labor Traficking/src/screens/AssessScreen.js
@@ -1,73 +1,117 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
-
-const ColoredLine = ({ color }) => (
-    <hr
-        style={{
-            color: color,
-            backgroundColor: color,
-            height: 3
-        }}
-    />
-);
+import {Dimensions, View} from 'react-native'    
+const DeviceWidth = Dimensions.get('window').width
 
 const AssessScreen = () => {
+
+    const centerBannerStyle = {
+        textAlign: "center",
+        fontSize: 16,
+        paddingBottom: "20px",
+        fontStyle: "italic"
+    };
+
     return (
 <>
             <div class="topBanner">
                 <img alt="topBanner" src="/icons/header.svg" class="image1"></img>
-                <img alt="result" src="/icons/AssessWordMark.svg" class="image2"></img>
-                
-
-                <hr className='yellowLine' />
-                <div class="description">
-                    <label>
-                    Questions designed for investigators to help determine if circumstances rise to the level of labor trafficking under Massachusetts law.
-                    </label>
-                </div>
+                <img alt="assess" src="/icons/AssessHeaderText.svg" class="image2"></img>
             </div>
+
             <div class="middleBanner">
+                <p style={centerBannerStyle}>Select a catagory to view questions, or  
+                <span style={{color: "#ff0000"}}> </span>
+                <span style={{color: "#20548c", textDecoration: "underline", fontStyle: "italic"}}>click here to download the questions</span>
+                </p>
             </div>
 
+    <View style={{
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }}>
+      
+    <View style={{
+      flexDirection: 'row',
+      backgroundColor: "grey"}}>
+      <View>
+        <View style={{width: DeviceWidth*0.3, height: DeviceWidth*0.3, marginBottom:2, backgroundColor: 'white'}}>
+            <h2 style={{ color: '#20548c' }}> Serious Harm</h2>
+            <p>Causes or threatens to cause serious harm to any person.</p>
+            <a href="/">VIEW QUESTIONS<i class="fas fa-arrow-right"></i> </a>
+        </View>
+        <View style={{width: DeviceWidth*0.3, height: DeviceWidth*0.3, marginTop:1, backgroundColor: 'white'}}>
+        <h2 style={{ color: '#20548c' }}>Extortion</h2>
+                        <p>Engages in extortion under Massachusetts Law.</p>
+                        <a href="/">VIEW QUESTIONS<i class="fas fa-arrow-right"></i> </a>
+        </View>
+      </View>
+      <View>
+        <View style={{width: DeviceWidth*0.3, height: DeviceWidth*0.3, marginBottom:2, marginLeft:2, backgroundColor: 'white'}}>
+        <h2 style={{ color: '#20548c' }}>Physical Restraint</h2>
+                        <p>Physically restrains or threatens to physically restrain another person.</p>
+                        <a href="/">VIEW QUESTIONS<i class="fas fa-arrow-right"></i> </a>
+        </View>
+        <View style={{width: DeviceWidth*0.3, height: DeviceWidth*0.3, marginTop:1, marginLeft:2, backgroundColor: 'white'}}>
+        <h2 style={{ color: '#20548c' }}>Financial Harm</h2>
+                        <p>Causes or threatens to cause financial harm to any person.</p>
+                        <a href="/">VIEW QUESTIONS<i class="fas fa-arrow-right"></i> </a>
+        </View>
+      </View>
+      <View>
+        <View style={{width: DeviceWidth*0.3, height: DeviceWidth*0.3, marginBottom:2, marginLeft:2, backgroundColor: 'white'}}>
+        <h2 style={{ color: '#20548c' }}>Abuse of Law</h2>
+                        <p>Abuses or threatens to abuse the law or legal process.</p>
+                        <a href="/">VIEW QUESTIONS<i class="fas fa-arrow-right"></i> </a>
+        </View>
+        <View style={{width: DeviceWidth*0.3, height: DeviceWidth*0.3, marginTop: 1, marginLeft:2, backgroundColor: 'white'}}>
+        <h2 style={{ color: '#20548c' }}>Identity Documents</h2>
+                        <p>Knowingly destroys, conceals, removes, confiscates or possesses any actual or purported government identification document, of another person.</p>
+                        <a href="/">VIEW QUESTIONS<i class="fas fa-arrow-right"></i> </a>
+        </View>
+      </View>    
+    </View>
+  </View>
+
+            {/* <div style={divStyle}>
             <Container>
                 <Row>
-                    <Col className='text py-3 vl'>
-                        <h2>Serious Harm</h2>
-                        <h4>Causes or threatens to cause serious harm to any person.</h4>
+                    <Col className='text py-3 vl border-right border-bottom'>
+                        <h2 style={{ color: '#20548c' }}>Serious Harm</h2>
+                        <p>Causes or threatens to cause serious harm to any person.</p>
                         <a href="/">VIEW QUESTIONS<i class="fas fa-arrow-right"></i> </a>
                     </Col>
-                    <Col className='text py-3 vl'>
-                        <h2>Physical Restraint</h2>
-                        <h4>Physically restrains or threatens to physically restrain another person.</h4>
+                    <Col className='text py-3 vl border-bottom'>
+                        <h2 style={{ color: '#20548c' }}>Physical Restraint</h2>
+                        <p>Physically restrains or threatens to physically restrain another person.</p>
                         <a href="/">VIEW QUESTIONS<i class="fas fa-arrow-right"></i> </a>
                     </Col>
-                    <Col className='text py-3'>
-                        <h2>Abuse of Law</h2>
-                        <h4>Abuses or threatens to abuse the law or legal process.</h4>
+                    <Col className='text py-3 border-bottom'>
+                        <h2 style={{ color: '#20548c' }}>Abuse of Law</h2>
+                        <p>Abuses or threatens to abuse the law or legal process.</p>
                         <a href="/">VIEW QUESTIONS<i class="fas fa-arrow-right"></i> </a>
                     </Col>
                 </Row>
 
-                <ColoredLine color="black" />
-
                 <Row>
                 <Col className='text py-3 vl'>
-                        <h2>Extortion</h2>
-                        <h4>Engages in extortion under Massachusetts Law. </h4>
+                        <h2 style={{ color: '#20548c' }}>Extortion</h2>
+                        <p>Engages in extortion under Massachusetts Law.</p>
                         <a href="/">VIEW QUESTIONS<i class="fas fa-arrow-right"></i> </a>
                     </Col>
                     <Col className='text py-3 vl'>
-                        <h2>Financial Harm</h2>
-                        <h4>Causes or threatens to cause financial harm to any person.</h4>
+                        <h2 style={{ color: '#20548c' }}>Financial Harm</h2>
+                        <p>Causes or threatens to cause financial harm to any person.</p>
                         <a href="/">VIEW QUESTIONS<i class="fas fa-arrow-right"></i> </a>
                     </Col>
                     <Col className='text py-3'>
-                        <h2>Identity Documents</h2>
-                        <h4>Knowingly destroys, conceals, removes, confiscates or possesses any actual or purported government identification document, of another person.</h4>
+                        <h2 style={{ color: '#20548c' }}>Identity Documents</h2>
+                        <p>Knowingly destroys, conceals, removes, confiscates or possesses any actual or purported government identification document, of another person.</p>
                         <a href="/">VIEW QUESTIONS<i class="fas fa-arrow-right"></i> </a>
                     </Col>
                 </Row>
             </Container>
+        </div> */}
         </>
     )
 }
