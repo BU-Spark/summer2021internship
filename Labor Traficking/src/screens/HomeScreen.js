9a04f20833f20d9ccf583797f195b0a13d2f5e25
jassulan <jassulan.expo@gmail.com>
2021-07-01 11:29:37 -0400

    HomeScreen Styling

--- Labor Traficking/src/screens/HomeScreen.js
+++ Labor Traficking/src/screens/HomeScreen.js
@@ -3,34 +3,34 @@ import React from 'react'
 const HomeScreen = () => {
     return (
         <>
-            <div class="topBanner">
-                <img alt="topBanner" src="/icons/header.svg" class="image1"></img>
-                <img alt="result" src="/icons/HeaderTextResult.svg" class="image2"></img>
+            <div className="topBanner">
+                <img alt="topBanner" src="/icons/header.svg" className="image1"></img>
+                <img alt="result" src="/icons/HeaderTextResult.svg" className="image2"></img>
             </div>
-            <div class="middleBanner">
+            <div className="middleBanner">
                 <h3>Our Tools</h3>
                 <div className="containers">
-                    <img alt="assess" src="/icons/Groupassess.svg" class="card__icon"></img>
+                    <img alt="assess" src="/icons/Groupassess.svg" className="card__icon"></img>
                     <h4>Assess</h4>
                     <hr className='yellowLine' />
                     <label>Determine if your case is labor trafficking</label>
-                    <a href="/">GET STARTED<i class="fas fa-arrow-right"></i> </a>
+                    <a href="/">GET STARTED<i className="fas fa-arrow-right"></i> </a>
                 </div>
                 <div className="containers">
-                    <img alt="prepare" src="/icons/Groupprepare.svg" class="card__icon"></img>
+                    <img alt="prepare" src="/icons/Groupprepare.svg" className="card__icon"></img>
                     <h4>Prepare</h4>
                     <hr className='yellowLine' />
                     <label>Find tips for interviewing victims</label>
-                    <a href="/">FIND TIPS<i class="fas fa-arrow-right"></i> </a>
+                    <a href="/">FIND TIPS<i className="fas fa-arrow-right"></i> </a>
                 </div>
 
 
                 <div className="containers" id="largeContainer">
-                    <img alt="resources" src="/icons/Groupresources.svg" class="card__icon"></img>
+                    <img alt="resources" src="/icons/Groupresources.svg" className="card__icon"></img>
                     <h4>Resources</h4>
                     <hr className='yellowLine' />
                     <label>Browse resources avaiable in your area</label>
-                    <a href="/">EXPLORE RESOURCES<i class="fas fa-arrow-right"></i> </a>
+                    <a href="/">EXPLORE RESOURCES<i className="fas fa-arrow-right"></i> </a>
                 </div>
             </div>
             <div className="bottomBanner">
@@ -39,7 +39,7 @@ const HomeScreen = () => {
                 <h5>
                     View the Massachusettes labor trafficking staute and stay informed.
                 </h5>
-                <button type="button" class="yellowBtn myFontColor">READ MORE</button>
+                <button type="button" className="yellowBtn myFontColor">READ MORE</button>
             </div>
         </>
     )
