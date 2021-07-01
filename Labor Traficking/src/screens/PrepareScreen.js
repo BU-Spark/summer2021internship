7f20251895b764fbdb8462cb147ff98c234711c5
jassulan <jassulan.expo@gmail.com>
2021-07-01 14:55:29 -0400

    'className' should be used instead of 'class'

--- Labor Traficking/src/screens/PrepareScreen.js
+++ Labor Traficking/src/screens/PrepareScreen.js
@@ -3,11 +3,11 @@ import React from 'react'
 const PrepareScreen = () => {
     return (
         <>
-            <div class="topBanner">
-                <img alt="topBanner" src="/icons/header.svg" class="image1"></img>
-                <img alt="prepare" src="/icons/HeaderTextPrepare.svg" class="image2"></img>
+            <div className="topBanner">
+                <img alt="topBanner" src="/icons/header.svg" className="image1"></img>
+                <img alt="prepare" src="/icons/HeaderTextPrepare.svg" className="image2"></img>
             </div>
-            
+
             <div>
                 Prepare
             </div>
