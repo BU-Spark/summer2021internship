2609db0015a4b6a7b137b5e520a1c36946e7517c
jassulan <jassulan.expo@gmail.com>
2021-07-13 08:42:54 -0400

    Page Massachusettes law

--- Labor Traficking/src/screens/MassLawScreen.js
+++ Labor Traficking/src/screens/MassLawScreen.js
@@ -3,7 +3,36 @@ import React from 'react'
 const MassLawScreen = () => {
     return (
         <div>
-            Massachusettes Law
+            <div className="topBanner">
+                <img alt="topBanner" src="/icons/header.svg" className="image1"></img>
+                <img alt="massLaw" src="/icons/HeaderTextMassLaw.svg" className="image2"></img>
+            </div>
+            <div className="middleBanner">
+                <h3>Massachusettes Law</h3>
+                <img alt="line" src="/icons/yellowline.svg" className="yellowLineSize"></img>
+                <div>
+                    <i>
+                        The Massachusetts anti-trafficking statute was enacted in 2012, and the law is still evolving. For those reasons, we often look for guidance from federal definitions and federal case law.
+                    </i>
+                </div>
+                <hr />
+                <div>
+                    <h4>Crime of Trafficking in Persons for Forced Services M.G.L. ch. 265, § 51 </h4>
+                </div>
+                <div>
+                    Under Massachusetts law, the crime of labor trafficking is known as Trafficking in Persons for Forced Services. This crime involves whoever knowingly:
+                    Subjects, or attempts to subject, another person to forced services, or recruits, entices, harbors, transports, provides or obtains by any means, or attempts to recruit, entice, harbor, transport, provide or obtain by any means, another person, intending or knowing that such person will be subjected to forced services; or
+                    Benefits, financially or by receiving anything of value, as a result of a violation of the above
+
+                </div>
+                <div>
+                    <h4>Definition of Forced Services M.G.L. ch. 265, § 49</h4>
+                </div>
+                <div>
+                    If one or more of the six prongs listed below are met, the conduct may be categorized as Trafficking in Persons for Forced Services.
+                    “Forced Services” is defined as services performed or provided by a person that are obtained or maintained by another person who:
+                </div>
+            </div>
         </div>
     )
 }
