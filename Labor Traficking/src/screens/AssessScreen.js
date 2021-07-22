2ba5106880ae63ba2dcba48c04cade4b4d6d32f1
Grace <gyoon18@bu.edu>
2021-07-22 08:24:15 -0400

    Finished Assess page

--- Labor Traficking/src/screens/AssessScreen.js
+++ Labor Traficking/src/screens/AssessScreen.js
@@ -14,6 +14,25 @@ const AssessScreen = () => {
         fontStyle: "italic"
     };
 
+    const gridPadding = {
+        marginLeft: '40px',
+        marginRight: '40px',
+        marginTop: '40px',
+        marginBottom: '40px'
+      };
+
+    const divStyle = {
+        marginLeft: '50px',
+        marginRight: '30px',
+        marginTop: '30px',
+        marginBottom: '50px'
+      };
+
+    const textStyle = {
+        marginTop: '30px',
+        marginBottom: '40px'
+    }
+
     return (
         <>
             <div className="topBanner">
@@ -28,60 +47,96 @@ const AssessScreen = () => {
                 </p>
             </div>
 
+        <div style={gridPadding}>
             <Container>
                 <Row>
                     <Col className="leftBox">
+                        <div style={divStyle}>
                         <div>
                             <img alt="seriousharm" src="/icons/SeriousHarm.svg"></img>
                         </div>
                         <img alt="yellowline" src="/icons/yellowline.svg"></img>
+                        <div style={textStyle}>
                         <label>Causes or threatens to cause serious harm to any person.</label>
+                        </div>
                         <a href="/">VIEW QUESTIONS<i className="fas fa-arrow-right"></i> </a>
+                        </div>
                     </Col>
                     <Col className="leftBox">
+                        <div style={divStyle}>
                         <div>
                             <img alt="physicalrestraint" src="/icons/PhysRestraint.svg"></img>
                         </div>
                         <img alt="yellowline" src="/icons/yellowline.svg"></img>
+                        <div style={textStyle}>
                         <label>Physically restrains or threatens to physically restrain another person.</label>
+                        </div>
+                        <div>
                         <a href="/">VIEW QUESTIONS<i className="fas fa-arrow-right"></i> </a>
+                        </div>
+                        </div>
                     </Col>
                     <Col className="rightBox">
+                        <div style={divStyle}>
                         <div>
                             <img alt="abuseoflaw" src="/icons/AbuseOfLaw.svg"></img>
                         </div>
                         <img alt="yellowline" src="/icons/yellowline.svg"></img>
+                        <div style={textStyle}>
                         <label>Abuses or threatens to abuse the law or legal process.</label>
+                        </div>
+                        <div>
                         <a href="/">VIEW QUESTIONS<i className="fas fa-arrow-right"></i> </a>
+                        </div>
+                        </div>
                     </Col>
                 </Row>
                 <Row>
                     <Col className="bottomBox">
+                        <div style={divStyle}>
                         <div>
                             <img alt="extortion" src="/icons/Extortion.svg"></img>
                         </div>
                         <img alt="yellowline" src="/icons/yellowline.svg"></img>
+                        <div style={textStyle}>
                         <label>Engages in extortion under Massachusetts Law. </label>
+                        </div>
+                        <div>
                         <a href="/">VIEW QUESTIONS<i className="fas fa-arrow-right"></i> </a>
+                        </div>
+                        </div>
                     </Col>
                     <Col className="bottomBox">
+                        <div style={divStyle}>
                         <div>
                             <img alt="financialharm" src="/icons/FinancialHarm.svg"></img>
                         </div>
                         <img alt="yellowline" src="/icons/yellowline.svg"></img>
+                        <div style={textStyle}>
                         <label>Causes or threatens to cause financial harm to any person.</label>
+                        </div>
+                        <div>
                         <a href="/">VIEW QUESTIONS<i className="fas fa-arrow-right"></i> </a>
+                        </div>
+                        </div>
                     </Col>
                     <Col>
+                        <div style={divStyle}>
                         <div>
                             <img alt="iddocuments" src="/icons/IdentityDocuments.svg"></img>
                         </div>
                         <img alt="yellowline" src="/icons/yellowline.svg"></img>
+                        <div style={textStyle}>
                         <label>Knowingly destroys, conceals, removes, confiscates or possesses any actual or purported government identification document, of another person.</label>
+                        </div>
+                        <div>
                         <a href="/">VIEW QUESTIONS<i className="fas fa-arrow-right"></i> </a>
+                        </div>
+                        </div>
                     </Col>
                 </Row>
             </Container>
+        </div>
         </>
     )
 }
