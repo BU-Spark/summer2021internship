97f65507342c27df95565c7fa8dcf27b93dc9aa1
jassulan <jassulan.expo@gmail.com>
2021-07-27 08:33:20 -0400

    MassLaw and AboutUs pages

--- Labor Traficking/src/screens/AboutUsScreen.js
+++ Labor Traficking/src/screens/AboutUsScreen.js
@@ -1,30 +1,57 @@
 import React from 'react'
-import { Accordion, Card } from 'react-bootstrap'
 
 const AboutUsScreen = () => {
     return (
         <div>
-            <Accordion>
-                <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="0">
-                        TAB 1
-                    </Accordion.Toggle>
+            <div className="topBanner">
+                <img alt="topBanner" src="/icons/header.svg" className="image1"></img>
+                <img alt="massLaw" src="/icons/HeaderTextAboutUs.svg" className="image2"></img>
+            </div>
+            <div className="middleBanner">
+                <div className="middleBanner">
+                    <h3>Why Us?</h3>
+                    <img alt="line" src="/icons/yellowline.svg" className="yellowLineSize"></img>
+                    <div>
+                        Because labor trafficking is everywhere. Because labor trafficking is difficult to see. Because we want to help answer your questions. Because we want victims on labor trafficking to know help is out there. Because we all have a role in unmasking labor trafficking.
 
-                    <Accordion.Collapse eventKey="0">
-                        <Card.Body>This is first tab body</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
+                        <b>Because the RESULT can aid in eradicating labor trafficking in Massachusetts.</b>
 
-                <Card>
-                    <Accordion.Toggle as={Card.Header} eventKey="1">
-                        TAB 2
-                    </Accordion.Toggle>
+                    </div>
+                </div>
 
-                    <Accordion.Collapse eventKey="1">
-                        <Card.Body>This is second tab body</Card.Body>
-                    </Accordion.Collapse>
-                </Card>
-            </Accordion>
+                <div className="middleBanner">
+                    <h3>Who We Are</h3>
+
+                    <img alt="line" src="/icons/yellowline.svg" className="yellowLineSize"></img>
+                    <div>
+                        A coalition of individuals focused on shining a light to bring labor trafficking out of the shadows.
+                    </div>
+
+                    <figure>
+                        <img src='/icons/icon_HTD.svg' alt='HTD' />
+                        <figcaption>Human Trafficking Division, <br />
+                            MA Office of the Attorney General</figcaption>
+                    </figure>
+
+                    <figure>
+                        <img src='/icons/icon_FLD.svg' alt='FLD' />
+                        <figcaption>Fair Labor Division, <br />
+                            MA Office of the Attorney General</figcaption>
+                    </figure>
+
+                    <figure>
+                        <img src='/icons/icon_Spark.svg' alt='HTD' />
+                        <figcaption>BU Spark!</figcaption> <br />
+
+                    </figure>
+
+                    <figure>
+                        <img src='/icons/icon_IRH.svg' alt='HTD' />
+                        <figcaption>Immigrants’ Rights and Human Trafficking Program, <br />
+                            BU School of Law</figcaption>
+                    </figure>
+                </div>
+            </div>
         </div>
     )
 }
--- Labor Traficking/src/screens/MassLawScreen.js
+++ Labor Traficking/src/screens/MassLawScreen.js
@@ -16,21 +16,70 @@ const MassLawScreen = () => {
                     </i>
                 </div>
                 <hr />
+
                 <div>
-                    <h4>Crime of Trafficking in Persons for Forced Services M.G.L. ch. 265, § 51 </h4>
+                    <h4>Crime of Trafficking in Persons for Forced Services</h4>
+                    <i>M.G.L. ch. 265, § 51</i>
+                    <br />
                 </div>
                 <div>
                     Under Massachusetts law, the crime of labor trafficking is known as Trafficking in Persons for Forced Services. This crime involves whoever knowingly:
-                    Subjects, or attempts to subject, another person to forced services, or recruits, entices, harbors, transports, provides or obtains by any means, or attempts to recruit, entice, harbor, transport, provide or obtain by any means, another person, intending or knowing that such person will be subjected to forced services; or
-                    Benefits, financially or by receiving anything of value, as a result of a violation of the above
+                    <ul>
+                        <li>Subjects, or attempts to subject, another person to forced services, or recruits, entices, harbors, transports, provides or obtains by any means, or attempts to recruit, entice, harbor, transport, provide or obtain by any means, another person, intending or knowing that such person will be subjected to forced services; or</li>
+                        <li>Benefits, financially or by receiving anything of value, as a result of a violation of the above</li>
+                    </ul>
+                </div>
 
+                <div>
+                    <h4>Definition of Forced Services</h4>
+                    <i>M.G.L. ch. 265, § 51</i>
+                    <br />
                 </div>
                 <div>
-                    <h4>Definition of Forced Services M.G.L. ch. 265, § 49</h4>
+                    If one or more of the six prongs listed below are met, the conduct may be categorized as Trafficking in Persons for Forced Services. “Forced Services” is defined as services performed or provided by a person that are obtained or maintained by another person who:
+                    <ul>
+                        <li>causes or threatens to cause serious harm to any person</li>
+                        <li>physically restrains or threatens to physically restrain another person</li>
+                        <li>abuses or threatens to abuse the law or legal processknowingly destroys, conceals, removes, confiscates or possesses any actual or purported passport or other immigration document, or any other actual or purported government identification document, of another person</li>
+                        <li>engages in extortion under Massachusetts law</li>
+                        <li>causes or threatens to cause financial harm to any person</li>
+                    </ul>
+                    “Services” are any act performed by a person under the supervision of or for the benefit of another including, but not limited to, commercial sexual activity and sexually explicit performances
+                </div>
+
+                <div>
+                    <h4>Overlap with Other Crimes</h4>
+                    <i>M.G.L. ch. 265, § 51</i>
+                    <br />
                 </div>
                 <div>
-                    If one or more of the six prongs listed below are met, the conduct may be categorized as Trafficking in Persons for Forced Services.
-                    “Forced Services” is defined as services performed or provided by a person that are obtained or maintained by another person who:
+                    Labor trafficking may overlap with other crimes, such as sex trafficking, sexual assault, wage theft, and other crimes. An investigator should be keep in mind that other violations of law may surface.
+                </div>
+
+                <div>
+                    <h4>Criminal Penalties</h4>
+                    <br />
+                </div>
+                <div>
+                    <ul>
+                        <li>The crime of Trafficking in Persons for Forced Services shall be punished by imprisonment in the state prison for not less than 5 years but not more than 20 years and by a fine of not more than $25,000. M.G.L. ch. 265, § 51(a).</li>
+                        <li>Whoever commits the crime of trafficking of persons for forced services upon a person under 18 years of age shall be punished by imprisonment in the state prison for life or for any term of years, but not less than 5 years. M.G.L. ch. 265, § 51(b).</li>
+                        <li>A business entity that commits trafficking of persons for forced labor services shall be punished by a fine of not more than $1,000,000.” M.G.L. ch. 265, § 51(c).</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h4>Federal Criminal Statutes</h4>
+                    <br />
+                </div>
+                <div>
+                    There are also federal criminal statutes that address labor trafficking crimes, such as forced labor and involuntary servitude. Click here to read more about the federal crimes of labor trafficking.
+
+                    Violations under federal law must be referred to federal authorities.
+
+                    To report to federal law enforcement, please contact the National Human Trafficking Resource Center Hotline at 1-888-373-7888, which will send the information to federal law enforcement within a given jurisdiction.
+
+                    Victims also may have additional rights to criminal restitution, civil remedies, and government benefits under federal law. Please refer victims to an attorney as soon as possible to explore their rights.
                 </div>
             </div>
         </div>
