fb6f3cd2dd40db84a10214438e5ceae8043d36ee
Grace <gyoon18@bu.edu>
2021-08-02 15:15:22 -0400

    Changed default font to Roboto to be consistent with design wireframes

--- Labor Traficking/src/screens/AssessSeriousHarm.js
+++ Labor Traficking/src/screens/AssessSeriousHarm.js
@@ -1,8 +1,9 @@
 import React from "react";
+import "../styles/AssessSeriousHarm.css";
 import { Container, Row, Col } from "react-bootstrap";
 import AssessNavigator from "../components/AssessNavigator";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
-import "../styles/AssessSeriousHarm.css";
+import Accordion from "react-bootstrap/Accordion";
 
 const AssessExtortion = () => {
   return (
@@ -12,7 +13,9 @@ const AssessExtortion = () => {
           <Col sm={2}>
             <AssessNavigator />
           </Col>
-          <Col>
+          {/* placeholder for spacing */}
+          <Col sm={1}></Col>
+          <Col xs={8}>
             <div>
               <ol class="breadcrumb">
                 <Breadcrumb>
@@ -51,18 +54,151 @@ const AssessExtortion = () => {
               services in order to avoid incurring that harm. 18 USC §
               1589(c)(2).
               <h2>Evaluate</h2>
-              <button class="accordion">Section 1</button>
-              <div class="panel">
-                <p>Lorem ipsum...</p>
-              </div>
-              <button class="accordion">Section 2</button>
-              <div class="panel">
-                <p>Lorem ipsum...</p>
-              </div>
-              <button class="accordion">Section 3</button>
-              <div class="panel">
-                <p>Lorem ipsum...</p>
-              </div>
+              <Accordion>
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>
+                    <Container>
+                      <Row>
+                        <h5>Question 1/9</h5>
+                      </Row>
+                      <Row>
+                        <p>
+                          Did the perpetrator physically hurt the worker or
+                          anyone else involved in the case?
+                        </p>
+                      </Row>
+                    </Container>
+                  </Accordion.Header>
+                  <Accordion.Body>mm tasty placeholder</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                  <Accordion.Header>
+                    <Container>
+                      <Row>
+                        <h5>Question 2/9</h5>
+                      </Row>
+                      <Row>
+                        <p>
+                          Did the perpetrator threaten to physically hurt the
+                          worker or anyone else?
+                        </p>
+                      </Row>
+                    </Container>
+                  </Accordion.Header>
+                  <Accordion.Body>mm yummy placeholder</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                  <Accordion.Header>
+                    <Container>
+                      <Row>
+                        <h5>Question 3/9</h5>
+                      </Row>
+                      <Row>
+                        <p>
+                          Did the perpetrator sexually assault the worker or
+                          threaten to sexually assault the worker?
+                        </p>
+                      </Row>
+                    </Container>
+                  </Accordion.Header>
+                  <Accordion.Body>mm yummy placeholder</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="4">
+                  <Accordion.Header>
+                    <Container>
+                      <Row>
+                        <h5>Question 4/9</h5>
+                      </Row>
+                      <Row>
+                        <p>
+                          Did the perpetrator threaten the worker with
+                          deportation?
+                        </p>
+                      </Row>
+                    </Container>
+                  </Accordion.Header>
+                  <Accordion.Body>mm yummy placeholder</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="5">
+                  <Accordion.Header>
+                    <Container>
+                      <Row>
+                        <h5>Question 5/9</h5>
+                      </Row>
+                      <Row>
+                        <p>
+                          Did the perpetrator threaten to contact the police for
+                          some sort of unlawful purpose?
+                        </p>
+                      </Row>
+                    </Container>
+                  </Accordion.Header>
+                  <Accordion.Body>mm yummy placeholder</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="6">
+                  <Accordion.Header>
+                    <Container>
+                      <Row>
+                        <h5>Question 6/9</h5>
+                      </Row>
+                      <Row>
+                        <p>
+                          Did the perpetrator threaten to fire the worker (not
+                          for good cause)?
+                        </p>
+                      </Row>
+                    </Container>
+                  </Accordion.Header>
+                  <Accordion.Body>mm yummy placeholder</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="7">
+                  <Accordion.Header>
+                    <Container>
+                      <Row>
+                        <h5>Question 7/9</h5>
+                      </Row>
+                      <Row>
+                        <p>
+                          Did the perpetrator threaten the worker with
+                          reputational harm?
+                        </p>
+                      </Row>
+                    </Container>
+                  </Accordion.Header>
+                  <Accordion.Body>mm yummy placeholder</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="8">
+                  <Accordion.Header>
+                    <Container>
+                      <Row>
+                        <h5>Question 8/9</h5>
+                      </Row>
+                      <Row>
+                        <p>
+                          Was the worker emotionally abused by the perpetrator?
+                        </p>
+                      </Row>
+                    </Container>
+                  </Accordion.Header>
+                  <Accordion.Body>mm yummy placeholder</Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="9">
+                  <Accordion.Header>
+                    <Container>
+                      <Row>
+                        <h5>Question 9/9</h5>
+                      </Row>
+                      <Row>
+                        <p>
+                          Did the perpetrator seriously harm or threaten the
+                          worker with serious harm in any other way?
+                        </p>
+                      </Row>
+                    </Container>
+                  </Accordion.Header>
+                  <Accordion.Body>mm yummy placeholder</Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
             </div>
           </Col>
         </Row>
@@ -70,25 +206,5 @@ const AssessExtortion = () => {
     </div>
   );
 };
-
-
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
 
 export default AssessExtortion;
