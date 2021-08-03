4ab84cfad59966c2f03463d9fe6a6ecc9f07d56b
Grace <gyoon18@bu.edu>
2021-08-03 08:13:36 -0400

    Adjusted appearance of Assess page

--- Labor Traficking/src/screens/AssessScreen.js
+++ Labor Traficking/src/screens/AssessScreen.js
@@ -1,117 +1,200 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../styles/AssessScreen.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import { View } from "react-native";
+import Col from "react-bootstrap/Col";
+import { Link } from "react-router-dom";
 
 const AssessScreen = () => {
+  return (
+    <>
+      <div className="topBanner">
+        <img alt="topBanner" src="/icons/header.svg" className="image1"></img>
+        <img
+          alt="assess"
+          src="/icons/AssessHeaderText.svg"
+          className="image2"
+        ></img>
+      </div>
 
-    return (
-        <>
-            <div className="topBanner">
-                <img alt="topBanner" src="/icons/header.svg" className="image1"></img>
-                <img alt="assess" src="/icons/AssessHeaderText.svg" className="image2"></img>
-            </div>
+      <div className="middleBanner">
+        <p
+          style={{
+            textAlign: "center",
+            fontFamily: "Roboto",
+            fontStyle: "italic",
+            fontWeight: "normal",
+            fontSize: 20,
+          }}
+        >
+          <span style={{ color: "##606263" }}>Click tabs to view tips or </span>
+          <span
+            style={{
+              color: "#20548c",
+              textDecoration: "underline",
+              fontStyle: "italic",
+            }}
+          >
+            click here to download the tips
+          </span>
+        </p>
+      </div>
 
-            <div className="middleBanner">
-                <p className="centerBannerStyle">Select a catagory to view questions, or
-                    <span style={{ color: "#ff0000" }}> </span>
-                    <span style={{ color: "#20548c", textDecoration: "underline", fontStyle: "italic" }}>click here to download the questions</span>
-                </p>
-            </div>
+      <div className="gridPadding">
+        <Container>
+          <Row>
+            <Col className="leftBox">
+              <div className="divStyle">
+                <h1 className="h1a">Serious Harm</h1>
+                <View
+                  style={{
+                    borderBottomColor: "#F4C346",
+                    borderBottomWidth: 3,
+                    height: -5,
+                    width: 90,
+                    marginBottom: 35,
+                  }}
+                />
+                <div className="textStyle">
+                  <label>
+                    Causes or threatens to cause serious harm to any person.
+                  </label>
+                </div>
+                <a href="/">
+                  VIEW QUESTIONS<i className="fas fa-arrow-right"></i>{" "}
+                </a>
+              </div>
+            </Col>
+            <Col className="leftBox">
+              <div className="divStyle">
+                <h1 className="h1a">Physical Restraint</h1>
+                <View
+                  style={{
+                    borderBottomColor: "#F4C346",
+                    borderBottomWidth: 3,
+                    height: -5,
+                    width: 90,
+                    marginBottom: 35,
+                  }}
+                />
+                <div className="textStyle">
+                  <label>
+                    Physically restrains or threatens to physically restrain
+                    another person.
+                  </label>
+                </div>
+                <div>
+                  <a href="/">
+                    VIEW QUESTIONS<i className="fas fa-arrow-right"></i>{" "}
+                  </a>
+                </div>
+              </div>
+            </Col>
+            <Col className="rightBox">
+              <div className="divStyle">
+                <h1 className="h1a">Abuse of Law</h1>
+                <View
+                  style={{
+                    borderBottomColor: "#F4C346",
+                    borderBottomWidth: 3,
+                    height: -5,
+                    width: 90,
+                    marginBottom: 35,
+                  }}
+                />
+                <div className="textStyle">
+                  <label>
+                    Abuses or threatens to abuse the law or legal process.
+                  </label>
+                </div>
+                <div>
+                  <a href="/">
+                    VIEW QUESTIONS<i className="fas fa-arrow-right"></i>{" "}
+                  </a>
+                </div>
+              </div>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="bottomBox">
+              <div className="divStyle">
+              <h1 className="h1a">Extortion</h1>
+                <View
+                  style={{
+                    borderBottomColor: "#F4C346",
+                    borderBottomWidth: 3,
+                    height: -5,
+                    width: 90,
+                    marginBottom: 35,
+                  }}
+                />
+                <div>
+                <div className="textStyle">
+                  <label>
+                  Engages in extortion under Massachusetts Law. 
+                  </label>
+                </div>
+                  <Link to="/assess/Extortion">VIEW QUESTIONS</Link>{" "}
+                  <i className="fas fa-arrow-right"></i>
+                </div>
+              </div>
+            </Col>
+            <Col className="bottomBox">
+              <div className="divStyle">
+              <h1 className="h1a">Financial Harm</h1>
+                <View
+                  style={{
+                    borderBottomColor: "#F4C346",
+                    borderBottomWidth: 3,
+                    height: -5,
+                    width: 90,
+                    marginBottom: 35,
+                  }}
+                />
+                <div>
+                <div className="textStyle">
+                  <label>
+                  Causes or threatens to cause financial harm to any person.
+                  </label>
+                </div>
+                  <a href="/">
+                    VIEW QUESTIONS<i className="fas fa-arrow-right"></i>{" "}
+                  </a>
+                </div>
+              </div>
+            </Col>
+            <Col>
+              <div className="divStyle">
+                <div>
+                <h1 className="h1a">Identity Documents</h1>
+                <View
+                  style={{
+                    borderBottomColor: "#F4C346",
+                    borderBottomWidth: 3,
+                    height: -5,
+                    width: 90,
+                    marginBottom: 35,
+                  }}
+                />
+                </div>
+                <div className="textStyle">
+                  <label>
+                  Knowingly destroys government identity document, of another person.
+                  </label>
+                </div>
+                <div>
+                  <a href="/">
+                    VIEW QUESTIONS<i className="fas fa-arrow-right"></i>{" "}
+                  </a>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
 
-            <div className="gridPadding">
-                <Container>
-                    <Row>
-                        <Col className="leftBox">
-                            <div className="divStyle">
-                                <div>
-                                    <img alt="seriousharm" src="/icons/SeriousHarm.svg"></img>
-                                </div>
-                                <img alt="yellowline" src="/icons/yellowline.svg"></img>
-                                <div className="textStyle">
-                                    <label>Causes or threatens to cause serious harm to any person.</label>
-                                </div>
-                                <a href="/">VIEW QUESTIONS<i className="fas fa-arrow-right"></i> </a>
-                            </div>
-                        </Col>
-                        <Col className="leftBox">
-                            <div className="divStyle">
-                                <div>
-                                    <img alt="physicalrestraint" src="/icons/PhysRestraint.svg"></img>
-                                </div>
-                                <img alt="yellowline" src="/icons/yellowline.svg"></img>
-                                <div className="textStyle">
-                                    <label>Physically restrains or threatens to physically restrain another person.</label>
-                                </div>
-                                <div>
-                                    <a href="/">VIEW QUESTIONS<i className="fas fa-arrow-right"></i> </a>
-                                </div>
-                            </div>
-                        </Col>
-                        <Col className="rightBox">
-                            <div className="divStyle">
-                                <div>
-                                    <img alt="abuseoflaw" src="/icons/AbuseOfLaw.svg"></img>
-                                </div>
-                                <img alt="yellowline" src="/icons/yellowline.svg"></img>
-                                <div className="textStyle">
-                                    <label>Abuses or threatens to abuse the law or legal process.</label>
-                                </div>
-                                <div>
-                                    <a href="/">VIEW QUESTIONS<i className="fas fa-arrow-right"></i> </a>
-                                </div>
-                            </div>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col className="bottomBox">
-                            <div className="divStyle">
-                                <div>
-                                    <img alt="extortion" src="/icons/Extortion.svg"></img>
-                                </div>
-                                <img alt="yellowline" src="/icons/yellowline.svg"></img>
-                                <div className="textStyle">
-                                    <label>Engages in extortion under Massachusetts Law. </label>
-                                </div>
-                                <div>
-                                    <Link to="/assess/Extortion">VIEW QUESTIONS</Link> <i className="fas fa-arrow-right"></i>
-                                </div>
-                            </div>
-                        </Col>
-                        <Col className="bottomBox">
-                            <div className="divStyle">
-                                <div>
-                                    <img alt="financialharm" src="/icons/FinancialHarm.svg"></img>
-                                </div>
-                                <img alt="yellowline" src="/icons/yellowline.svg"></img>
-                                <div className="textStyle">
-                                    <label>Causes or threatens to cause financial harm to any person.</label>
-                                </div>
-                                <div>
-                                    <a href="/">VIEW QUESTIONS<i className="fas fa-arrow-right"></i> </a>
-                                </div>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div className="divStyle">
-                                <div>
-                                    <img alt="iddocuments" src="/icons/IdentityDocuments.svg"></img>
-                                </div>
-                                <img alt="yellowline" src="/icons/yellowline.svg"></img>
-                                <div className="textStyle">
-                                    <label>Knowingly destroys, conceals, removes, confiscates or possesses any actual or purported government identification document, of another person.</label>
-                                </div>
-                                <div>
-                                    <a href="/">VIEW QUESTIONS<i className="fas fa-arrow-right"></i> </a>
-                                </div>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        </>
-    )
-}
-
-export default AssessScreen
+export default AssessScreen;
