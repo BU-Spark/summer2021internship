b82d033ff136b0cc7ca0f876e3296ca4a20fa13b
jassulan <jassulan.expo@gmail.com>
2021-09-02 15:16:05 -0400

    AssessExtortion Modal Form

--- Labor Traficking/src/screens/AssessExtortion.js
+++ Labor Traficking/src/screens/AssessExtortion.js
@@ -9,6 +9,7 @@ import AssessNavigator from "../components/AssessNavigator";
 import Breadcrumb from "react-bootstrap/Breadcrumb";
 import Accordion from "react-bootstrap/Accordion";
 import styled from "styled-components";
+import Modal from 'react-bootstrap/Modal';
 
 <link
   href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900"
@@ -16,11 +17,32 @@ import styled from "styled-components";
 />;
 
 const AssessExtortion = () => {
-  const [value, setValue] = React.useState("Yes");
+  const [q01, setQuestion01] = React.useState('');
+  const [q02, setQuestion02] = React.useState('');
+  const [q03, setQuestion03] = React.useState('');
+  const [q04, setQuestion04] = React.useState('');
 
-  const handleChange = (event) => {
-    setValue(event.target.value);
+  const [showSH, setShowSH] = React.useState(false);
+  const [showDisc, setShowDisc] = React.useState(false);
+  const [showUnclear, setShowUnclear] = React.useState(false);
+
+  const handleClose = () => {
+    setShowSH(false);
+    setShowDisc(false);
+    setShowUnclear(false);
   };
+
+  const handleShow = () => {
+    if (q01 === 'Yes' || q02 === 'Yes' || q03 === 'Yes' || q04 === 'Yes') {
+      setShowSH(true);
+    } else if (q01 === '' || q02 === '' || q03 === '' || q04 === '') {
+      setShowDisc(true);
+    }
+    else {
+      setShowUnclear(true);
+    };
+  };
+
 
   const Button = styled.button`
     background-color: #144d7e;
@@ -76,7 +98,7 @@ const AssessExtortion = () => {
               </div>
               <div className="definitionHeader">
                 <h2 className="h2">Examples:</h2>
-                <div className="definition" style={{lineHeight: '40px'}}>
+                <div className="definition" style={{ lineHeight: '40px' }}>
                   <ul>
                     <li>An employer threatens to release embarrassing photographs, unless the worker continues to work.</li>
                   </ul>
@@ -134,7 +156,7 @@ const AssessExtortion = () => {
                       </div>
                       <Accordion.Body>
                         <div className="radioAlign">
-                          <RadioGroup row value={value} onChange={handleChange}>
+                          <RadioGroup row value={q01} onChange={event => setQuestion01(event.target.value)}>
                             <div className="yesButton">
                               <FormControlLabel
                                 value="Yes"
@@ -179,7 +201,7 @@ const AssessExtortion = () => {
                       </Accordion.Header>
                       <Accordion.Body>
                         <div className="radioAlign">
-                          <RadioGroup row value={value} onChange={handleChange}>
+                          <RadioGroup row value={q02} onChange={event => setQuestion02(event.target.value)}>
                             <div className="yesButton">
                               <FormControlLabel
                                 value="Yes"
@@ -223,7 +245,7 @@ const AssessExtortion = () => {
                       </Accordion.Header>
                       <Accordion.Body>
                         <div className="radioAlign">
-                          <RadioGroup row value={value} onChange={handleChange}>
+                          <RadioGroup row value={q03} onChange={event => setQuestion03(event.target.value)}>
                             <div className="yesButton">
                               <FormControlLabel
                                 value="Yes"
@@ -267,7 +289,7 @@ const AssessExtortion = () => {
                       </Accordion.Header>
                       <Accordion.Body>
                         <div className="radioAlign">
-                          <RadioGroup row value={value} onChange={handleChange}>
+                          <RadioGroup row value={q04} onChange={event => setQuestion04(event.target.value)}>
                             <div className="yesButton">
                               <FormControlLabel
                                 value="Yes"
@@ -302,8 +324,60 @@ const AssessExtortion = () => {
       </Container>
 
       <div className="button">
-        <Button>EVALUATE</Button>
+        <Button onClick={handleShow}>EVALUATE</Button>
       </div>
+
+      <Modal show={showSH} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <i class='fas fa-exclamation-triangle fa-2x'></i>
+          <h1>Serious Harm</h1>
+
+          There appear to be likely indicators of labor trafficking. Continue evaluation or consider referring to law enforcement at this time.
+
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            RE-EVALUATE
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showUnclear} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <i class="fa fa-question-circle fa-2x"></i>
+          <h1>Unclear</h1>
+          It is unclear if this is a case of labor trafficking. Continue evaluation in this or other categories or consider referring to law enforcement at this time.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            RE-EVALUATE
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal show={showDisc} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title></Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h1>Disclaimer</h1>
+          Your answers will not be saved if you leave this page. We recommend that you finish the evaluation to determine the result before exiting.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleClose}>
+            CONTINUE EVALUATION
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            LEAVE PAGE
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
